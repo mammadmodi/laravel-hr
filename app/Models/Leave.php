ffefa359fad3457620a90807da16117c710fb450
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2019-07-20 11:41:21 +0430

    Create Leave model and table.

---
+++ app/Models/Leave.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class Leave
+ *
+ * @package App\Models
+ * @property int $id
+ * @property int $user_id
+ * @property int $committer_id
+ * @property $start
+ * @property $end
+ * @property string $status
+ * @property User $user
+ * @property User $committer
+ */
+class Leave extends Model
+{
+    const STATUS_CREATED = 'created';
+    const STATUS_WAIT_FOR_APPROVE = 'wait_for_approve';
+    const STATUS_APPROVED = 'approved';
+    const STATUS_REJECTED = 'rejected';
+    const STATUS_CANCELED = 'canceled';
+
+    protected $fillable = [
+        'start',
+        'end'
+    ];
+
+    protected $hidden = [
+        'user_id',
+        'committer_id'
+    ];
+
+    /**
+     * Get the user that requested this leave.
+     *
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get the user that has committed this leave.
+     *
+     * @return BelongsTo
+     */
+    public function committer()
+    {
+        return $this->belongsTo(User::class, 'committer_id');
+    }
+}
---
+++ database/migrations/2019_07_20_064143_create_leaves_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Leave;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeavesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('committer_id')->nullable();
+            $table->timestamp('start');
+            $table->timestamp('end');
+            $table->string('status')->default(Leave::STATUS_WAIT_FOR_APPROVE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leaves');
+    }
+}
