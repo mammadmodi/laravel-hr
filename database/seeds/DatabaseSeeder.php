794231b70bb57472fa0b7cc72fc9d020927227a4
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2019-07-21 16:22:56 +0430

    Add PermissionsTableSeeder to DatabaseSeeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call(RolesTableSeeder::class);
          $this->call(DepartmentsTableSeeder::class);
          $this->call(UsersTableSeeder::class);
+         $this->call(PermissionsTableSeeder::class);
     }
 }
