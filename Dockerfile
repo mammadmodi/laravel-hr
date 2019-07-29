16ddc6fdac23721ce31532bae52ce3bf7b226faf
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2019-07-29 17:57:28 +0430

    Remove road runner.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,13 @@ FROM registry.snapp.tech/docker/php:7.2-cli-alpine3.8
 WORKDIR /app
 RUN apk update && \
     apk add php7-redis &&\
-    apk add wget
+    apk add wget &&\
 
-RUN php -m
+RUN wget https://github.com/spiral/roadrunner/releases/download/v1.3.5/roadrunner-1.3.5-linux-amd64.tar.gz\
+    && tar -xzvf roadrunner-1.3.5-linux-amd64.tar.gz \
+    && cp ./roadrunner-1.3.5-linux-amd64/rr /usr/local/bin/ \
+    && cp ./roadrunner-1.3.5-linux-amd64/rr /app
 COPY . /app
+RUN rm -rf ./roadrunner-1.3.5-linux-amd64/ && rm ./roadrunner-1.3.5-linux-amd64.tar.gz
+RUN php -m
+CMD ["rr", "serve", "-vd"]
---
+++ psr-worker.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+
+$relay = new Spiral\Goridge\StreamRelay(STDIN, STDOUT);
+$psr7 = new Spiral\RoadRunner\PSR7Client(new Spiral\RoadRunner\Worker($relay));
+$httpFoundationFactory = new HttpFoundationFactory();
+
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+while ($req = $psr7->acceptRequest()) {
+    try {
+        $request = Illuminate\Http\Request::createFromBase($httpFoundationFactory->createRequest($req));
+        $response = $app->dispatch($request);
+
+        $psr7factory = new DiactorosFactory();
+        $psr7response = $psr7factory->createResponse($response);
+        $psr7->respond($psr7response);
+    } catch (\Throwable $e) {
+        $psr7->getWorker()->error((string)$e);
+    }
+}
