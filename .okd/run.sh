6e284e900571c06d2778f5aab90b8e4aee6a6870
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2019-07-31 09:15:26 +0430

    Update Docker supervisor file.

--- .okd/run.sh
+++ .okd/run.sh
@@ -9,5 +9,5 @@ oc process -f Service.yaml --ignore-unknown-parameters=true | oc apply -f -
 echo "processing deployment config..."
 oc process -f DeploymentConfig.yaml --ignore-unknown-parameters=true | oc apply -f -
 
-#echo "processing route..."
-#oc process -f Route.yaml --param-file=.env --ignore-unknown-parameters=true | oc apply -f -
+echo "processing route..."
+oc process -f Route.yaml --ignore-unknown-parameters=true | oc apply -f -
