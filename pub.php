16a103fdf67781581ac1dddf6ae771296fc82488
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2019-08-13 14:32:42 +0430

    Add test stage requirements.

---
+++ pub.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection(
+    "rabbitmq",
+    5672,
+    "user35T",
+    "kaq46srwL6awLna0"
+);
+$channel = $connection->channel();
+
+$channel->queue_declare('notification', false, false, false, false);
+
+$data = implode(' ', array_slice($argv, 1));
+if (empty($data)) {
+    $data = "Hello World!";
+}
+$msg = new AMQPMessage(
+    $data,
+    array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+);
+
+$channel->basic_publish($msg, '', 'notification');
+
+echo ' [x] Sent ', $data, "\n";
+
+$channel->close();
+$connection->close();
