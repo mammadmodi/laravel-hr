00ee8c34a026c86572e28ea5f6ef3830b7480acd
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2019-08-13 14:12:36 +0430

    Add test stage requirements.

--- consume.php
+++ consume.php
@@ -15,7 +15,7 @@ $channel = $connection->channel();
 
 
 
-$channel->queue_declare('task_queue', false, true, false, false);
+$channel->queue_declare('notification', false, false, false, false);
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
@@ -27,7 +27,7 @@ $callback = function ($msg) {
 };
 
 $channel->basic_qos(null, 1, null);
-$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+$channel->basic_consume('notification', '', false, false, false, false, $callback);
 
 while ($channel->is_consuming()) {
     $channel->wait();
